95c89f7c2be5c12fb28bcfbfe7a7c9a383c9b85c
Sudhir Yadav <sudhirkyadav51@gmail.com>
2024-03-21 17:49:12 +0530

    calculator
    this is calculator files.

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-let userInput=document.getElementById('userInput')
-const buttom=document.querySelector('button')
-//const result=document.getElementById('displayResult')
-let generated=document.getElementById('generated')
-const numberGenerater=function(){
-    number=Math.floor(Math.random()*10)
-    console.log(number);
-    generated.value=number;// if you have to display Your Value on display 
-    return number;
+const display=document.getElementById('display')
+const displayNO=function(input){
+    display.value += input;
+ }
+const Clear= function(){
+    display.value="";
+ }
+const calculate=function(){
+    display.value=eval(display.value)
 }
-
-buttom.addEventListener("click",function(){
-        let number=numberGenerater()
-        if (userInput.value==number) {
-            console.log("You are Lucky");
-        } else {
-            console.log("Try again");
-        }
-    
-})
